4f0b7ca9cd76d6ab25b3a527aee12707db9004a1
Tarun Jain <tjain73@yahoo.com>
2021-02-10 13:46:30 -0500

    Day 2_2

---
+++ day2_2.fsx
@@ -0,0 +1,27 @@
+open System.IO
+
+type PasswordPolicy = {
+    min: int;
+    max: int;
+    letter: char;
+    password: string
+}
+
+let parseData (line: string) = 
+    let z = line.Replace("-", " ").Replace(":", "").Split(" ")
+    { min= int z.[0]; max= int z.[1]; letter=char z.[2]; password= z.[3] }
+
+let checkPasswordPolicy policy =     
+    let charCount = policy.password 
+                        |> String.filter (fun c -> c = policy.letter)
+                        |> String.length
+
+    charCount >= policy.min && charCount <= policy.max
+
+let data = File.ReadLines("day_2_1_data.txt") 
+            |> Array.ofSeq
+            |> Array.map parseData
+            |> Array.filter checkPasswordPolicy
+            |> Array.length
+
+printfn $"{data}"
